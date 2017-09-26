b71bc55179faaebc9d4813ecdde6c8ba3aa6375b
Nathan Menge <nathan.menge@arcadiasolutions.com>
2017-09-26 16:12:02 -0400

    Added install of aws-cli (and pip3 if needed) if not already installed.

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,18 @@ if [ ! -f ~/.ssh/config ]; then
     touch ~/.ssh/config
 fi
 
+if [ ! $(which aws) ]; then
+    if [ ! $(which pip3) ]; then
+        brew install python3
+        echo "Python3 installed"
+    fi
+    pip3 install awscli --upgrade --user
+    echo "================================"
+    echo "Add the following to .bash_profile"
+    echo "export PATH=$(python3 -m site --user-base)/bin:\$PATH"
+    echo "================================"
+fi
+
 #deploy all user root files
 for f in $USER_ROOT_FILES 
 do
