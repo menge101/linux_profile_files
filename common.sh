a425a188e71b2ff81ea1c29886be218934bbe954
Nathan Menge <nathan.menge@arcadiasolutions.com>
2017-10-20 10:25:05 -0400

    [Git ignore] - Added Global git ignore file

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 #defintion of files to deploy to user root
-USER_ROOT_FILES="startup.py .bashrc .bashrc_remote .bash_aliases .bash_functions .bash_completion_functions .bash_profile .gitconfig .profile .vimrc"
+USER_ROOT_FILES="startup.py .bashrc .bashrc_remote .bash_aliases .bash_functions .bash_completion_functions .bash_profile .gitconfig .gitignore_global .profile .vimrc"
 
 #definition of files to deploy to autocomplete folder
 BASH_COMPLETION_FILES="rake.sh sshs.sh"
